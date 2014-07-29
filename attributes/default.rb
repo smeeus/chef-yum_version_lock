1d4b607675fa41c424789880c6522c046d256d3f
Jonathan Serafini <jonathan@lightspeedretail.com>
2014-07-28 20:14:27 -0400

    Add immediate installation option

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 
 default[:yum][:versionlock][:file] = "/etc/yum/pluginconf.d/versionlock.list"
+default[:yum][:versionlock][:runtime] = false
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,9 @@
 
-package "yum-plugin-versionlock"
+r = package "yum-plugin-versionlock" do
+  action :nothing
+end
+
+if node[:yum][:versionlock][:compile_time] then r.run_action :install
+else r.action :install
+end
 
