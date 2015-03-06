f5e74adbb211c5db5c04fa77ef00615cede3309f
smeeus <sven.meeus@kangaroot.net>
2015-03-06 17:15:21 +0100

    Debugging code

--- providers/default.rb
+++ providers/default.rb
@@ -72,14 +72,27 @@ def version_locks
         parsed_data << hash
       end
     end
+
+    # debugging
+    puts "version_locks - parsed_data: #{parsed_data}"
+
     parsed_data
   end
 end
 
 def version_locks_for_hash opts={}
-  version_locks.select do |data|
+
+  # debugging
+  puts "version_locks_for_hash - opts: #{opts}"
+
+  ret = version_locks.select do |data|
     opts.all? { |k,v| data[k] == v }
   end
+
+  # debugging
+  puts "version_locks_for_hash - ret: #{ret}"
+
+  ret
 end
 
 def version_locks_for_resource
@@ -88,50 +101,76 @@ end
 
 def version_locks_outstanding
   resource_hash = hash_from_resource
-
-  # debugging
-  puts "version_locks_outstanding - resource_hash: #{resource_hash}"
-
   resource_hash.delete :version
-
-  # debugging
-  puts "version_locks_outstanding - resource_hash: #{resource_hash}"
-
   resource_hash.delete :release
-
-  # debugging
-  puts "version_locks_outstanding - resource_hash: #{resource_hash}"
-
 
   version_locks_for_hash(resource_hash).select do |hash|
 
-    # debugging
-    puts "version_locks_outstanding - version_locks_for_hash(resource_hash).select: #{hash}"
-    puts "version_locks_outstanding - new_resource: #{new_resource}"
+#    # debugging
+#    puts "version_locks_outstanding - version_locks_for_hash(resource_hash).select: #{hash}"
+#    puts "version_locks_outstanding - new_resource.version: #{new_resource.version}"
+#    puts "version_locks_outstanding - new_resource.release: #{new_resource.release}"
 
     next if hash[:version] == new_resource.version
 
+#    # debugging
+#    puts "version_locks_outstanding - next if hash[:version] == new_resource.version"
+
     if hash[:version] == new_resource.version
+
+#      # debugging
+#      puts "version_locks_outstanding - if hash[:version] == new_resource.version"
+
       if new_resource.release and hash[:release]
+
+#        # debugging
+#        puts "version_locks_outstanding - if new_resource.release and hash[:release]"
+
         next if hash[:release] == new_resource.release
+
+#        # debugging
+#        puts "version_locks_outstanding - next if hash[:release] == new_resource.release"
+
       end
     end
     
+#    # debugging
+#    puts "version_locks_outstanding - true"
+
     true
   end
+
 end
 
 action :lock do
+  puts ""
+  puts ""
+
+  # debugging
+  puts "action :lock - version_locks_outstanding: #{version_locks_outstanding}"
+
   # Unlock outstanding version locks
   version_locks_outstanding.uniq.each do |hash|
+
+    # debugging
+    puts "action :lock - version_locks_outstanding.uniq.each: #{hash}"
+    puts "action :lock - spec_from_hash hash: #{spec_from_hash hash}"
+
     execute "yum versionlock delete #{spec_from_hash hash}" do
       action :nothing
     end.run_action :run
     new_resource.updated_by_last_action true
   end
 
+  # debugging
+  puts "action :lock - version_locks_for_resource.empty?: #{version_locks_for_resource}"
+
   # Lock current version if not locked
   if version_locks_for_resource.empty?
+
+    # debugging
+    puts "action :lock - spec_from_hash hash_from_resource: #{spec_from_hash hash_from_resource}"
+
     execute "yum versionlock add #{spec_from_hash hash_from_resource}" do
       action :nothing
     end.run_action :run
@@ -140,6 +179,9 @@ action :lock do
 end
 
 action :unlock do
+  puts ""
+  puts ""
+
   # Remove locks matching attributes supplied
   version_locks_for_resource.uniq.each do |hash|
     execute "yum versionlock delete #{spec_from_hash hash}" do
