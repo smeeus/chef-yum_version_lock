a3e076f091bce7fd839f3f80a89b0fc5259274f5
smeeus <sven.meeus@kangaroot.net>
2015-03-09 15:34:21 +0100

    Add support for epoch and arch values

--- providers/default.rb
+++ providers/default.rb
@@ -103,40 +103,18 @@ def version_locks
         parsed_data << hash
       end
     end
-
-#    # debugging
-#    puts "version_locks - parsed_data: #{parsed_data}"
-
     parsed_data
   end
 end
 
 def version_locks_for_hash opts={}
-
-#  # debugging
-#  puts "version_locks_for_hash - opts: #{opts}"
-
-  ret = version_locks.select do |data|
+  version_locks.select do |data|
     opts.all? { |k,v| data[k] == v }
   end
-
-#  # debugging
-#  puts "version_locks_for_hash - ret: #{ret}"
-
-  ret
 end
 
 def version_locks_for_resource
-
-#  # debugging
-#  puts "version_locks_for_resource - hash_from_resource: #{hash_from_resource}"
-
-  ret = version_locks_for_hash hash_from_resource
-
-#  # debugging
-#  puts "version_locks_for_resource - ret: #{ret}"
-
-  ret
+  version_locks_for_hash hash_from_resource
 end
 
 def version_locks_outstanding
@@ -147,84 +125,45 @@ def version_locks_outstanding
   resource_hash.delete :arch
 
   version_locks_for_hash(resource_hash).select do |hash|
-
-#    # debugging
-#    puts "version_locks_outstanding - version_locks_for_hash(resource_hash).select hash: #{hash}"
-#    puts "version_locks_outstanding - new_resource.epoch: #{new_resource.epoch}"
-#    puts "version_locks_outstanding - new_resource.version: #{new_resource.version}"
-#    puts "version_locks_outstanding - new_resource.release: #{new_resource.release}"
-#    puts "version_locks_outstanding - new_resource.arch: #{new_resource.arch}"
-#    puts "version_locks_outstanding - hash[:epoch]: #{hash[:epoch]}"
-#    puts "version_locks_outstanding - hash[:version]: #{hash[:version]}"
-#    puts "version_locks_outstanding - hash[:release]: #{hash[:release]}"
-#    puts "version_locks_outstanding - hash[:arch]: #{hash[:arch]}"
-
     donext = true
 
     if new_resource.version and hash[:version]
       if hash[:version] != new_resource.version
         donext = false 
-
-#        # debugging
-#        puts "version_locks_outstanding - donext hash[:version] != new_resource.version: #{donext}"
-
       end
     else
       if new_resource.release and hash[:release]
         if hash[:release] != new_resource.release
           donext = false
-
-#          # debugging
-#          puts "version_locks_outstanding - donext hash[:release] != new_resource.release: #{donext}"
-
         end
       else
         if new_resource.epoch and hash[:epoch]
           if hash[:epoch] != new_resource.epoch
             donext = false 
-
-#            # debugging
-#            puts "version_locks_outstanding - donext hash[:epoch] != new_resource.epoch: #{donext}"
-
           end
         else
           if new_resource.arch and hash[:arch]
             if hash[:arch] != new_resource.arch
               donext = false 
-
-#              # debugging
-#              puts "version_locks_outstanding - donext hash[:arch] != new_resource.arch: #{donext}"
-
             end
           else
             donext = false
-
-#            # debugging
-#            puts "version_locks_outstanding - donext not new_resource.arch and hash[:arch]: #{donext}"
-
           end
         end
       end
     end
 
-#    # debugging
-#    puts "version_locks_outstanding - donext: #{donext}"
-
     next if donext
 
     true
-
   end
+
 end
 
 
 action :lock do
   # Unlock outstanding version locks
   version_locks_outstanding.uniq.each do |hash|
-
-#    # debugging
-#    puts "action :lock - version_locks_outstanding.uniq.each hash: #{hash}"
-
     execute "yum versionlock delete #{spec_from_hash hash}" do
       action :nothing
     end.run_action :run
